bf8135bb2cf3e277a69291417620ec8b5734c121
Vikash-Kumar-Rana <vikashsonidi@gmail.com>
2021-01-28 12:52:56 +0530

    2 files modified

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 
-
 @Component({
   selector: "app-nav-bar",
   templateUrl: "./nav-bar.component.html",
@@ -9,15 +8,21 @@ import { Component, OnInit } from "@angular/core";
 export class NavBarComponent implements OnInit {
   active = 1;
   links = [
-    { title: "One", fragment: "one" },
-    { title: "Two", fragment: "two" }
+    { id: 1, link: "VendorBills", name: "Vendor Bills" },
+    { id: 2, link: "DriverWages", name: "DriverWages & CMPF" },
+    { id: 3, link: "WorkOrders", name: "Work Orders" },
+    { id: 4, link: "Vehicles", name: "Vehicles" },
+    { id: 5, link: "VendorBills", name: "Vendor Bills" },
+    { id: 6, link: "Vendors", name: "Vendors" },
+    { id: 7, link: "Drivers", name: "Drivers" },
+    { id: 8, link: "Files", name: "Files" }
   ];
 
   constructor() {}
 
   ngOnInit() {}
   navbarOpen = false;
- 
+
   toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
   }
